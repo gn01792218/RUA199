afce2001d987560aafa8bc76f0b17f3463f3cac9
jacky <jacky@metallurgietech.com>
2022-09-16 13:17:59 +0800

    調整首頁第三部分RWD

--- src/components/homePage/TheHomePageSection3.vue
+++ src/components/homePage/TheHomePageSection3.vue
@@ -1,23 +1,25 @@
 <template>
-    <section id="home-sction3" class="flex-xy-center py-20 relative">
+    <section id="home-sction3" class="flex-xy-center py-20 md:px-11 lg:px-20 relative">
         <div class="max-w-[960px]">
-            <article class="w-full relative mt-36 mb-20">
-                <img class="absolute absolute-x-center w-[310px] top-[-120%] sm:w-auto sm:translate-x-[0px] sm:left-0 bottom-[-25%]"
+            <article class="w-full mt-36 mb-20 flex flex-col sm:flex-row items-center">
+                <img class="w-[310px] translate-y-[10%] sm:w-[350px] md:w-[430px] lg:w-auto sm:translate-y-[-10%]"
                     :src="getAssetsFileURL('images/amazing-games 1.png')" alt="amazing-game">
-                <div class="sm:h-[143px] sm:w-[60%] ml-auto bg-gradient-1 rounded-[5px] flex-xy-center py-6">
-                    <h1 class="title-tertiary text-center">{{$t('homePage.section3.title')}}</h1>
-                    <p class="w-1/2 description text-left">{{$t('homePage.section3.description')}}</p>
+                <div class="sm:h-[143px] sm:w-[600px] sm:ml-[-5%] bg-gradient-1 rounded-[5px] flex-xy-center py-6">
+                    <div class="w-[50%] text-center sm:text-left">
+                        <h1 class="title-tertiary">{{$t('homePage.section3.title')}}</h1>
+                        <p class="description">{{$t('homePage.section3.description')}}</p>
+                    </div>
                 </div>
             </article>
             <article class="w-full relative">
-                <p class="title-secondary hidden sm:block">Over <span class="text-[50px]">2,000+</span>Games</p>
-                <div class="sm:h-[143px] bg-gradient-1 rounded-[5px] pt-20 pb-6 px-10 sm:pt-6">
-                    <div class="flex justify-start items-center sm:pr-32">
-                        <p class="max-w-[355px] description mr-2">{{$t('homePage.section3.description1')}}</p>
-                        <p class="max-w-[355px] description">{{$t('homePage.section3.description2')}}</p>
+                <p class="title-secondary hidden md:block">Over <span class="text-[50px]">2,000+</span>Games</p>
+                <div class="bg-gradient-1 rounded-[5px] p-10 md:p-5 lg:pb-6 lg:px-10 md:h-[143px]">
+                    <div class="flex flex-col justify-start items-center mt-10 md:mt-0 sm:flex-row md:pr-24 lg:pr-32">
+                        <p class="w-full description mb-5 md:mb-0 sm:mr-2 sm:max-w-[355px]">{{$t('homePage.section3.description1')}}</p>
+                        <p class="w-full description sm:max-w-[355px]">{{$t('homePage.section3.description2')}}</p>
                     </div>
                 </div>
-                <img class="absolute w-[125px] bottom-[85%] right-[50%] translate-x-[50%] sm:w-auto sm:right-[8%] sm:bottom-[10%]"
+                <img class="absolute w-[125px] bottom-[80%] right-[50%] translate-x-[50%] sm:bottom-[70%]  md:w-auto md:right-[8%] md:bottom-[10%]"
                     :src="getAssetsFileURL('images/caishen-a2 1.png')" alt="money-god">
             </article>
         </div>
