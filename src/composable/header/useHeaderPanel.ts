b4bdf0802f7c886ddaa1bd8c30adc35e41873d48
jacky <jacky@metallurgietech.com>
2022-09-20 15:21:32 +0800

    移除不必要的防滑代碼

--- src/composable/header/useHeaderPanel.ts
+++ src/composable/header/useHeaderPanel.ts
@@ -4,16 +4,13 @@ export default function useHeaderPanel() {
     
 function handleHeaderPanel() {
     show.value = !show.value
-    let mo = function(e:Event) {e.preventDefault()}
     let app = document.getElementById("app") as HTMLElement
     if(show.value) {
         document.body.style.overflow = 'hidden'
         app.style.overflow = 'hidden'
-        // document.addEventListener("touchmove",mo,false)
     }else {
         document.body.style.overflow = 'auto'
         app.style.overflow = 'auto'
-        // document.removeEventListener("touchmove",mo,false)
     }
 }
     return {
