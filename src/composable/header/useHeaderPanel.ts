5d6cadff71d5cec1b0e93b125fac192d9365fd21
jacky <jacky@metallurgietech.com>
2022-09-20 14:58:02 +0800

    修正桌機版本防滑機制

--- src/composable/header/useHeaderPanel.ts
+++ src/composable/header/useHeaderPanel.ts
@@ -5,12 +5,15 @@ export default function useHeaderPanel() {
 function handleHeaderPanel() {
     show.value = !show.value
     let mo = function(e:Event) {e.preventDefault()}
+    let app = document.getElementById("app") as HTMLElement
     if(show.value) {
         document.body.style.overflow = 'hidden'
-        document.addEventListener("touchmove",mo,false)
+        app.style.overflow = 'hidden'
+        // document.addEventListener("touchmove",mo,false)
     }else {
         document.body.style.overflow = 'auto'
-        document.removeEventListener("touchmove",mo,false)
+        app.style.overflow = 'auto'
+        // document.removeEventListener("touchmove",mo,false)
     }
 }
     return {
